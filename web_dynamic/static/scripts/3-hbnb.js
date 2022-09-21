2e93699dc8a3ccc6bfc47e35dd1af835a7d34642
Mpqg <mapazquiga@gmail.com>
2022-09-21 17:48:09 -0500

    Sending a POST

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -1,28 +1,51 @@
-$(document).ready(init);
+$('document').ready(function () {
+    $.get('http://0.0.0.0:5001/api/v1/status/', function (data, textStatus) {
+        if (textStatus === 'success') {
+            if (data.status === 'OK') {
+                $('#api_status').addClass('available');
+            } else {
+                $('#api_status').removeClass('available');
+            }
+        }
+    });
 
-const HOST = '0.0.0.0';
-
-function init() {
-    const amenityObj = {};
-    $('.amenities .popover input').change(function () {
+    let amenities = {};
+    $('input[type="checkbox"]').change(function () {
         if ($(this).is(':checked')) {
-            amenityObj[$(this).attr('data-name')] = $(this).attr('data-id');
-        } else if ($(this).is(':not(:checked)')) {
-            delete amenityObj[$(this).attr('data-name')];
-        }
-        const names = Object.keys(amenityObj);
-        $('.amenities h4').text(names.sort().join(', '));
-    });
-    api_status();
-}
-
-function api_status() {
-    const API_URL = `http://0.0.0.0:5001/api/v1/places_search/`;
-    $.get(API_URL, (data, textStatus) => {
-        if (textStatus == 'sucess' && data.status == 'OK') {
-            $('#api_status').addClass('available');
+            amenities[$(this).attr('data-id')] = $(this).attr('data-name');
         } else {
-            $('api_status').removeClass('available');
+            delete amenities[$(this).attr('data-id')];
         }
+        $('.amenities h4').text(Object.values(amenities).join(', '));
     });
-}
+});
+
+
+const places_search = `http://0.0.0.0:5001/api/v1/places_search/`
+$.ajax({
+    url: places_search,
+    type: 'POST',
+    contentType: 'application/json',
+    dataType: 'json',
+    data: '{}',
+    success: function (data) {
+        for (let i of data) {
+            let places = [
+                `<article>`,
+                `<div class="title_box">`,
+                `<h2>${ i.name }</h2>`,
+                `<div class="price_by_night">${ i.price_by_night }</div>`,
+                `</div>`,
+                `<div class="information">`,
+                `<div class="max_guest">${ i.max_guest } Guests</div>`,
+                `<div class="number_rooms">${ i.number_rooms } Bedrooms</div>`,
+                `<div class="number_bathrooms">${ i.number_bathrooms } Bathrooms</div>`,
+                `</div>`,
+                `<div class="description">`,
+                `${ i.description }`,
+                `</div>`,
+                ` </article>`];
+            $('.places').append(places.join(''));
+        }
+    }
+})
